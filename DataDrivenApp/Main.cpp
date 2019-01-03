e9a831c21c19fe1a5471788580ddb12a4b3aefca
SophC7289 <sophie.curtis17@bathspa.ac.uk>
2019-01-03 21:21:57 +0000

    Update Main.cpp
    Started Else if for second section of the menu

--- DataDrivenApp/Main.cpp
+++ DataDrivenApp/Main.cpp
@@ -12,18 +12,18 @@ int main(){
 	//Prints to the console window what happens and tells the user to press a number to find out specific tweets
 	cout << "This program will look through 1000's of tweets from the top 20 twitter users. Pick a number from the list below to find specific tweets.." << endl;
 	cout << "1 - exact number of tweets collated" << endl; //This will search every tweet and give them the exact number of tweets in the file
-	cout << "2 - that mention money" << endl; //Search every tweet that mentions money
-	cout << "3 - that mention politics" << endl; //Search every tweet that mentions politics
-	cout << "4 - mentioning the word Paris" << endl; //Search every tweet that mentions/uses the word "Paris"
-	cout << "5 - mentioning the word DreamWorks" << endl; //Search every tweet that mentions/used the word "DreamWorks" 
-	cout << "6 - mentioning the word Uber" << endl; //Search every tweet that mentions/uses the word "Uber"
+	cout << "2 - mentioning money" << endl; //Search every tweet that mentions money
+	cout << "3 - mentioning politics" << endl; //Search every tweet that mentions politics
+	cout << "4 - mentioning the word 'Paris'" << endl; //Search every tweet that mentions/uses the word "Paris"
+	cout << "5 - mentioning the word 'DreamWorks'" << endl; //Search every tweet that mentions/used the word "DreamWorks" 
+	cout << "6 - mentioning the word 'Uber'" << endl; //Search every tweet that mentions/uses the word "Uber"
 
 	cin >> input; //User input
 
 	//Each number in the menu is included in one big if statement 
 	if (input == 1) { //User presses 1 
 		
-		//This opens the file in write mode
+		//This opens the file in read mode
 		ifstream inFile;
 		inFile.open("sampleTweets.csv"); //The sample tweet file
 
@@ -39,6 +39,14 @@ int main(){
 		else {
 			cout << "There is an error reading the tweet file" << endl; //If the file hasn't been read then this message will come up 
 		}
+
+	}
+	else if (input == 2) { //If the user enters 2
+		
+		//This opens the file in read mode
+		ifstream inFile;
+		inFile.open("sampleTweets.csv"); //The sample tweet file
+
 
 	}
 	system("pause");
