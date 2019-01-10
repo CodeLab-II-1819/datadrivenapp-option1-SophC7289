64caa4b11a39d6102b95f2450860e65e1d0a0fe4
SophC7289 <sophie.curtis17@bathspa.ac.uk>
2019-01-10 14:29:03 +0000

    Reszing the window
    The user will not be able to resize the console window when loaded

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW); //How big I want the console window to be 
-
-	
-	ofRunApp(new ofApp());
+	//ofSetupOpenGL(1024,768,OF_WINDOW); //Screen size of the console window
+	ofGLFWWindowSettings settings;
+	settings.resizable = false;
+	settings.setSize(800, 700);
+	ofCreateWindow(settings);
+	return ofRunApp(new ofApp());
 
 }
