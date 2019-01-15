637ad6a7455f32e6836e5d017f34a475a1252f84
SophC7289 <sophie.curtis17@bathspa.ac.uk>
2019-01-15 17:55:26 +0000

    While loop
    While loop finsished and brek in the code. All commented

--- DataDrivenApp/Main.cpp
+++ DataDrivenApp/Main.cpp
@@ -9,51 +9,60 @@ int main(){
 	int count = 0; //Declaring variable number of tweets in sample file
 	string Tweets; //Declaring tweet variable 
 
-	//Prints to the console window what happens and tells the user to press a number to find out specific tweets
-	cout << "This program will look through 1000's of tweets from the top 20 twitter users. Pick a number from the list below to find specific tweets.." << endl;
-	cout << "1 - exact number of tweets collated" << endl; //This will search every tweet and give them the exact number of tweets in the file
-	cout << "2 - mentioning money" << endl; //Search every tweet that mentions money
-	cout << "3 - mentioning politics" << endl; //Search every tweet that mentions politics
-	cout << "4 - mentioning the word 'Paris'" << endl; //Search every tweet that mentions/uses the word "Paris"
-	cout << "5 - mentioning the word 'DreamWorks'" << endl; //Search every tweet that mentions/used the word "DreamWorks" 
-	cout << "6 - mentioning the word 'Uber'" << endl; //Search every tweet that mentions/uses the word "Uber"
+	while (input != 0) {
+		//Prints to the console window what happens and tells the user to press a number to find out specific tweets
+		cout << "This program will look through 1000's of tweets from the top 20 twitter users. Pick a number from the list below to find specific tweets.." << endl;
+		cout << "1 - exact number of tweets collated" << endl; //This will search every tweet and give them the exact number of tweets in the file
+		cout << "2 - mentioning money" << endl; //Search every tweet that mentions money
+		cout << "3 - mentioning politics" << endl; //Search every tweet that mentions politics
+		cout << "4 - mentioning the word 'Paris'" << endl; //Search every tweet that mentions/uses the word "Paris"
+		cout << "5 - mentioning the word 'DreamWorks'" << endl; //Search every tweet that mentions/used the word "DreamWorks" 
+		cout << "6 - mentioning the word 'Uber'" << endl; //Search every tweet that mentions/uses the word "Uber"
 
-	cin >> input; //User input
+		cin >> input; //User input
 
-	//Each number in the menu is included in one big if statement 
-	if (input == 1) { //User presses 1 
-		
-		//This opens the file in read mode
-		ifstream inFile;
-		inFile.open("sampleTweets.csv"); //The sample tweet file
+		//Each number in the menu is included in one big if statement 
+		if (input == 1) { //User presses 1 
 
-		if (inFile.good()) { //Checks the file is open
+			//This opens the file in read mode
+			ifstream inFile;
+			inFile.open("sampleTweets.csv"); //The sample tweet file
 
-			while (!inFile.eof()) { //This while loop will keep looking through the file until it reaches the end
-				getline(inFile, Tweets);
-				count++; //Counts every tweet 1 by 1  
+			if (inFile.good()) { //Checks the file is open
+
+				while (!inFile.eof()) { //This while loop will keep looking through the file until it reaches the end
+					getline(inFile, Tweets);
+					count++; //Counts every tweet 1 by 1  
+				}
+				cout << "There are " << count << " lines of tweets being read." << endl; //Prints this line with the exact number of tweets in the file
+				inFile.close(); //Closes the file
 			}
-			cout << "There are " << count << " lines of tweets being read." << endl; //Prints this line with the exact number of tweets in the file
-			inFile.close(); //Closes the file
+			else {
+				cout << "There is an error reading the tweet file" << endl; //If the file hasn't been read then this message will come up 
+			}
+
 		}
-		else {
-			cout << "There is an error reading the tweet file" << endl; //If the file hasn't been read then this message will come up 
-		}
+		else if (input == 2) { //If the user enters 2
 
-	}
-	else if (input == 2) { //If the user enters 2
-		
-		//This opens the file in read mode
-		ifstream inFile;
-		inFile.open("sampleTweets.csv"); //The sample tweet file
+			//This opens the file in read mode
+			ifstream inFile;
+			inFile.open("sampleTweets.csv"); //The sample tweet file
 
-		if (inFile.good()) { //checks if the file is open
+			if (inFile.good()) { //checks if the file is open
 
-			while (!inFile.eof()) { //Will keep running until it reaches the end of the file 
+				while (!inFile.eof()) { //Will keep running until it reaches the end of the file 
 
+				}
 			}
 		}
-	}
+		cout << "To carry on press enter to finish press 0" << endl;
+		int cont; //Temporary variable so when it gets to "system("cls")" it won't close straight away
+		cin >> cont; //Waits for the user to press enter to carry on or press 0 to quit
+		system("cls"); //Clears the console window
+
+	} 
+	cout << "Thanks for using this. Goodbye" << endl; //The final message when the user presses 0
+	
 	system("pause");
 	return 0;
 }
