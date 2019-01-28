f08eaba4e458cdc3f900b4a8c91d7c8a3b70247a
SophC7289 <sophie.curtis17@bathspa.ac.uk>
2019-01-28 18:16:09 +0000

    GUI
    Thought GUI was done, realised that I forgot the "Uber" category in the menu

--- gui/src/ofApp.cpp
+++ gui/src/ofApp.cpp
@@ -17,15 +17,16 @@ void ofApp::draw(){
 	ofSetColor(145, 196, 242); //Sets the menu shape colour (inner shape)
 	ofDrawRectRounded(21, 151, 224, 524, 10); //Menu shape size and position (inner shape) (x, y, w, h, rounded)
 	ofSetColor(229, 91, 42); //This colour represents what category the user has selected 
-	myText.drawString("How many...?", 30, 190); //"How many...?" category in menu
+	myText.drawString("How many...?", 30, 183); //"How many...?" category in menu
 	ofSetColor(45, 163, 213); //Text colour for everything else
-	myText.drawString("Money", 30, 250); //"Money" category in menu
-	myText.drawString("Politics", 30, 310); //"Politics" category in menu
-	myText.drawString("Paris", 30, 370); //"Paris" category in menu
-	myText.drawString("Dreamworks", 30, 430); //"Dreamworks" category in menu
-	myText.drawString("Youtube", 30, 490); //"Youtube" category in menu
-	myText.drawString("Animal", 30, 550); //"Animal" category in menu
-	myText.drawString("Music", 30, 610); //"Music" category in menu
+	myText.drawString("Money", 30, 235); //"Money" category in menu
+	myText.drawString("Politics", 30, 290); //"Politics" category in menu
+	myText.drawString("Paris", 30, 345); //"Paris" category in menu
+	myText.drawString("Dreamworks", 30, 400); //"Dreamworks" category in menu
+	myText.drawString("Uber", 30, 455); //"Uber" category in menu
+	myText.drawString("Youtube", 30, 510); //"Youtube" category in menu
+	myText.drawString("Animal", 30, 565); //"Animal" category in menu
+	myText.drawString("Music", 30, 620); //"Music" category in menu
 	myText.drawString("Llama", 30, 670); //"Llama" category in menu
 	ofSetColor(0, 185, 174); //Outer shape colour to give the main box a outline
 	ofDrawRectRounded(261, 148, 518, 531, 10); //Main box shape and position (outer)
